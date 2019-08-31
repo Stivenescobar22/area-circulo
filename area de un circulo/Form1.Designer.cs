310d42e3a36a99c0103186704ed4650c86f659f5
Stivenescobar22 <estibenes@gmail.com>
2019-08-31 15:37:31 -0500

    se agregan los archivos faltantes para el proyecto

---
+++ area de un circulo/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace area_de_un_circulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lblradio = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtradio = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(342, 269);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(92, 37);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.Btncalcular_Click);
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(247, 26);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(258, 24);
+            this.lbltitulo.TabIndex = 1;
+            this.lbltitulo.Text = "calculo del area de un circulo";
+            // 
+            // lblradio
+            // 
+            this.lblradio.AutoSize = true;
+            this.lblradio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblradio.Location = new System.Drawing.Point(248, 116);
+            this.lblradio.Name = "lblradio";
+            this.lblradio.Size = new System.Drawing.Size(66, 20);
+            this.lblradio.TabIndex = 2;
+            this.lblradio.Text = "Radio :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(211, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado : ";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // txtradio
+            // 
+            this.txtradio.Location = new System.Drawing.Point(323, 113);
+            this.txtradio.Name = "txtradio";
+            this.txtradio.Size = new System.Drawing.Size(127, 20);
+            this.txtradio.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(323, 189);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(127, 20);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtradio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblradio);
+            this.Controls.Add(this.lbltitulo);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lblradio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtradio;
+        private System.Windows.Forms.TextBox txtresultado;
+    }
+}
+
---
+++ area de un circulo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace area_de_un_circulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btncalcular_Click(object sender, EventArgs e)
+        {
+
+            string radioc = txtradio.Text;
+            string resultadoc = txtresultado.Text;
+
+            float radio = float.Parse(radioc);
+            float total = 3.1415F * (radio * radio);
+           
+
+            txtresultado.Text = total.ToString();
+
+
+            
+        }
+    }
+}
